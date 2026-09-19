0607fa7962dc7b6eff19bb2e20c7df3a5b7f8812
MohdSayam <sayamansari78300@gmail.com>
2026-09-19 18:46:34 +0530

    fix: use dynamic socket.io connection url instead of hardcoded localhost

--- frontend/src/views/InterviewRoom.vue
+++ frontend/src/views/InterviewRoom.vue
@@ -385,7 +385,8 @@ export default {
     },
 
     connectSignaling() {
-      this.socket = io("http://127.0.0.1:5000", {
+      const baseUrl = (import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:5000/api").replace(/\/api\/?$/, "")
+      this.socket = io(baseUrl, {
         transports: ["websocket", "polling"]
       })
 
